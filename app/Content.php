8a9a69611834ce89f35a68877e6af7233ef7142b
GENTAXXX <bramantyaraka00@mail.ugm.ac.id>
2020-02-20 16:08:08 +0700

    Pertemuan Pertama

---
+++ app/Content.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model 
+{
+    public $timestamps = false;
+    protected $primarykey = 'id_content';
+    protected $table = 'content';
+    protected $fillable = [
+        'title', 'text',
+    ];
+    
+
+
+}
+
